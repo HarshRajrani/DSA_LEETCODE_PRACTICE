b690c1ff03182d80a3731cc92a249b84a6b3a926
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-03-01 02:06:54 +0530

    Time: 47 ms (54.00%) | Memory: 8.2 MB (41.33%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int M = 1e9+7;
     int concatenatedBinary(int n) {
-        
         long result = 0;
-        
-        for(int i = 1; i<=n; i++) {
-            int digits = log2(i) + 1;
-            
-            result = (((result << digits)%M) + i)%M;
+        int digits = 0;
+
+        for(int num = 1; num <= n; num++) {
+            //digit count tabhi barhta hai jab power of 2 ata hai
+            if((num & (num-1)) == 0)
+                digits++;
+
+            result = ((result << digits)%M + num)%M;
         }
-        
+
         return result;
-        
     }
 };
