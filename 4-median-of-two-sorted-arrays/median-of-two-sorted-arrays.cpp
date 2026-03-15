e2085ea41f0cee43f1261b171cb5a0c8fe2c260f
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-03-15 17:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (94.29%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,26 +3,62 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
+        int size = m+n;
         
-        vector<int> temp(m+n);
+        int idx1 = (size/2)-1;
+        int element1 = -1;
+        int idx2 = size/2;
+        int element2 = -1;
+        
         int i = 0, j = 0, k = 0;
         
         while(i < m && j < n) {
-            if(nums1[i] < nums2[j])
-                temp[k++] = nums1[i++];
-            else
-                temp[k++] = nums2[j++];
+            if(nums1[i] < nums2[j]) {
+                if(k == idx1) {
+                    element1 = nums1[i];
+                }
+                if(k == idx2) {
+                    element2 = nums1[i];
+                }
+                i++;
+            } else {
+                if(k == idx1) {
+                    element1 = nums2[j];
+                }
+                if(k == idx2) {
+                    element2 = nums2[j];
+                }
+                j++;
+            }
+            k++;
         }
         
-        while(i < m) temp[k++] = nums1[i++];
-        while(j < n) temp[k++] = nums2[j++];
+        while(i < m) {
+            if(k == idx1) {
+                element1 = nums1[i];
+            }
+            if(k == idx2) {
+                element2 = nums1[i];
+            }
+            i++;
+            k++;
+        }
         
-        int size = m+n;
+        while(j < n) {
+            if(k == idx1) {
+                element1 = nums2[j];
+            }
+            if(k == idx2) {
+                element2 = nums2[j];
+            }
+            j++;
+            k++;
+        }
+
+        if(size%2 == 1)
+            return element2;
         
-        if(size%2 != 0)
-            return temp[size/2];
-        
-        return (temp[size/2]+temp[(size/2)-1])/2.0;
+        return (element1 + element2)/2.0;
         
     }
 };
