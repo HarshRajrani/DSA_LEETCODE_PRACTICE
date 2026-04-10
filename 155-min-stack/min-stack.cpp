a71636c3b708fcc466eacc0716366cc89b6258d1
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-04-10 12:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (83.76%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,51 +1,52 @@
 class MinStack {
-    private:
-        // Use long long to prevent overflow during the 2*val - mini calculation
-        stack<long long> st;
-        long long mini;
+private:
+    // Use long long to prevent overflow during the 2*val - mini calculation
+    stack<long long> st;
+    long long mini;
 
-    public:
-        MinStack() {}
+public:
+    MinStack() {}
 
-        void push(int val) {
-            long long value = val;
-            if (st.empty()) {
+    void push(int val) {
+        long long value = val;
+        if (st.empty()) {
+            st.push(value);
+            mini = value;
+        } else {
+            if (value >= mini) {
                 st.push(value);
-                mini = value;
             } else {
-                if (value >= mini) {
-                    st.push(value);
-                } else {
-                    // Encoding the new minimum
-                    st.push(2 * value - mini);
-                    mini = value;
-                }
+                // Encoding the new minimum
+                st.push(2 * value - mini);
+                mini = value;
             }
         }
+    }
 
-        void pop() {
-            if (st.empty()) return;
+    void pop() {
+        if (st.empty())
+            return;
 
-            long long x = st.top();
-            st.pop();
+        long long x = st.top();
+        st.pop();
 
-            // If x < mini, it means x was an encoded value
-            // We must retrieve the previous minimum
-            if (x < mini) {
-                mini = 2 * mini - x;
-            }
+        // If x < mini, it means x was an encoded value
+        // We must retrieve the previous minimum
+        if (x < mini) {
+            mini = 2 * mini - x;
         }
+    }
 
-        int top() {
-            if (st.empty()) return -1;
-            long long x = st.top();
-            
-            // If top is less than mini, mini IS the actual value
-            if (x < mini) return (int)mini;
-            return (int)x;
-        }
+    int top() {
+        if (st.empty())
+            return -1;
+        long long x = st.top();
 
-        int getMin() {
+        // If top is less than mini, mini IS the actual value
+        if (x < mini)
             return (int)mini;
-        }
+        return (int)x;
+    }
+
+    int getMin() { return (int)mini; }
 };
