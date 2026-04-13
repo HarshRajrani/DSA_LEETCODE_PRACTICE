2c1d8c6e71c6cefec66d23964b488e0e37953168
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-04-14 03:45:42 +0530

    Time: 19 ms (37.20%) | Memory: 47.3 MB (21.25%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,97 +1,80 @@
 class Solution {
-private :
+private:
+    vector<int> findNSE(vector<int>& arr) {
 
-    vector<int> findNSE(vector<int> &arr) {
-        
-    
         int n = arr.size();
-        
-        
+
         vector<int> ans(n);
-        
- 
+
         stack<int> st;
-        
-      
-        for(int i = n - 1; i >= 0; i--) {
-            
-            
+
+        for (int i = n - 1; i >= 0; i--) {
+
             int currEle = arr[i];
-            
-            /* Pop the elements in the stack until 
-            the stack is not empty and the top 
+
+            /* Pop the elements in the stack until
+            the stack is not empty and the top
             element is not the smaller element */
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
                 st.pop();
             }
-            
-          
+
             ans[i] = !st.empty() ? st.top() : n;
-            
-           
+
             st.push(i);
         }
-        
-   
+
         return ans;
     }
-    
 
-    vector<int> findPSEE(vector<int> &arr) {
-        
-        
+    vector<int> findPSEE(vector<int>& arr) {
+
         int n = arr.size();
-        
+
         vector<int> ans(n);
-        
-   
+
         stack<int> st;
-        
-        for(int i=0; i < n; i++) {
-            
-            
+
+        for (int i = 0; i < n; i++) {
+
             int currEle = arr[i];
-            
-          
-            while(!st.empty() && arr[st.top()] > arr[i]){
+
+            while (!st.empty() && arr[st.top()] > arr[i]) {
                 st.pop();
             }
-            
-           
+
             ans[i] = !st.empty() ? st.top() : -1;
-            
-          
+
             st.push(i);
         }
-        
-  
+
         return ans;
     }
 
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        vector<int> nse = findNSE(arr) ;
+        vector<int> nse = findNSE(arr);
 
-         vector<int> psee = findPSEE(arr) ;
+        vector<int> psee = findPSEE(arr);
 
-         int n = arr.size() ;
+        int n = arr.size();
 
-         int mod = 1e9 + 7 ; 
+        int mod = 1e9 + 7;
 
-         int sum = 0 ; 
+        int sum = 0;
 
-         for(int i = 0 ; i< n ; i++){
-            int left = i - psee[i] ; 
+        for (int i = 0; i < n; i++) {
+            int left = i - psee[i];
 
-            int right = nse[i] - i ; 
-             
-             long long freq = left*right*1LL ; 
+            int right = nse[i] - i;
 
-             int val = (freq*arr[i]*1ll) % mod ; 
+            long long freq = left * right * 1LL;
 
-             sum = ( sum + val ) % mod ; 
-         }
+            int val = (freq * arr[i] * 1ll) % mod;
 
-         return sum ; 
+            sum = (sum + val) % mod;
+        }
+
+        return sum;
     }
 };
