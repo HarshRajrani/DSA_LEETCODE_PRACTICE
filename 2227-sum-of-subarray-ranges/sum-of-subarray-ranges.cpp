c5eafc04329c2d9a13dbe1da84c2158cb0881158
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-04-23 17:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (90.04%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-
-        int n = nums.size();
-
-        long long ans = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            int maxi = nums[i];
-            int mini = nums[i];
-            for (int j = i; j < n; j++) {
-
-                mini = min(mini, nums[j]);
-                maxi = max(maxi, nums[j]);
-
-                ans += maxi - mini;
+    long long subArrayRanges(vector<int>& n) {
+        return sumSubarrayComp(n, less<int>()) -
+               sumSubarrayComp(n, greater<int>());
+    }
+    long long sumSubarrayComp(vector<int>& n, function<bool(int, int)> comp) {
+        long long res = 0;
+        vector<int> s;
+        for (int i = 0; i <= n.size(); ++i) {
+            while (!s.empty() && (i == n.size() || comp(n[s.back()], n[i]))) {
+                int j = s.back(), k = s.size() < 2 ? -1 : s[s.size() - 2];
+                res += (long long)(i - j) * (j - k) * n[j];
+                s.pop_back();
             }
+            s.push_back(i);
         }
-
-        return ans;
+        return res;
     }
 };
