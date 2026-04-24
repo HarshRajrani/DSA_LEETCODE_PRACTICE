71839c504af1fd8ea9753de1df65c882ce4e2da6
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-04-24 17:49:55 +0530

    Time: 4 ms (2.33%) | Memory: 9.1 MB (30.54%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        
+        int n = moves.size() ;     
+        
+        int countR = 0 ; int countL = 0 ; int t = 0 ; 
+
+        for(int i = 0 ; i<n ; i++){
+            if(moves[i] == 'L') countL++;
+            else if(moves[i] == 'R') countR++ ;
+            else t++ ;
+        }
+
+        return countL >= countR ? abs(countL + t - countR ) : countR + t - countL ; 
+    }
+};
