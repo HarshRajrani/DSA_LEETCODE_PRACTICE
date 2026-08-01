dfb0452b6c8fc1b0eeeff8ba45d1d0093aacb9da
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-08-02 02:01:24 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (78.42%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,7 +4,7 @@ public:
         int n = height.size() ;
         int water =0, left =0, right = n-1, lmax =0, rmax = 0;
      while( left <right){
-        if ( height[left]< height[right]){
+        if ( height[left]<= height[right]){
              lmax = max(lmax, height[left]);
             water += lmax-height[left];
             left ++;
