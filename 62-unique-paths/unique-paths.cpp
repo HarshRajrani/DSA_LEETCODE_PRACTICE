aea56785f2028927d0f7024888ed9ef19037414a
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-08-04 00:04:13 +0530

    Time: 1 ms (26.88%) | Memory: 9.5 MB (30.85%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -7,7 +7,7 @@ int solve(int i , int j , int m , int n ,  vector<vector<int>> &dp){
 
     if(i >= m || j>= n) return 0 ; 
 
-    if(i == m-1 && j == n-1) return 1 ;//dp[i][j] =1 ;
+    if(i == m-1 && j == n-1) return dp[i][j] =1 ;
 
     if(dp[i][j] != -1 ) return dp[i][j] ;
 
