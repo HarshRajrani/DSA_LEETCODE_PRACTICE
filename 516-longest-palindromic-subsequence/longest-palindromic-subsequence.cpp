89178661e9a22ac383eeb60a7c5a0975855f0d3a
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-08-22 21:26:11 +0530

    Time: 82 ms (46.99%) | Memory: 76.1 MB (8.16%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-int dp[1001][1001];
-   int solve(string &s , string &s2 , int i , int j ){
+    int solve(string& s, string& s2, int i, int j, vector<vector<int>>& dp) {
+        //  base case
 
-    // base case
+        if (i < 0 || j < 0)
+            return 0;
 
-    if(i < 0 || j<0) return 0 ;
-    
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
+        int taken = 0;
+        int not_taken = 0;
 
-    if(dp[i][j] != -1 ) return dp[i][j] ;
-    //condition
-    int take=0,skip=0;
-    if(s[i]==s2[j]){
-        take= 1 +solve(s,s2,i-1,j-1);
+        if (s[i] == s2[j])
+            taken = 1 + solve(s, s2, i - 1, j - 1, dp);
+
+        else
+            not_taken =
+                max(solve(s, s2, i - 1, j, dp), solve(s, s2, i, j - 1, dp));
+
+        return dp[i][j] = max(taken, not_taken);
     }
-    else{
-        skip= max(solve(s,s2,i-1,j),solve(s,s2,i,j-1));
-    }
-    return dp[i][j]= max(skip,take);
-   }
-
-
     int longestPalindromeSubseq(string s) {
-        string s2 =s;
-        reverse(s2.begin(),s2.end());
-       
-       memset(dp , -1 , sizeof(dp)) ;
 
-       return solve(s, s2 , s.size()-1 , s2.size()-1);
+        int n = s.size();
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        int m = s2.size();
 
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return solve(s, s2, n - 1, m - 1, dp);
     }
 };
