8bd8d45111250b5637265ba966417e628325aa10
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-08-30 06:30:03 +0530

    Time: 535 ms (22.56%) | Memory: 296.5 MB (22.22%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        
-        int n = nums.size() ;
-        vector<int> arr ; 
-        arr.push_back(nums[0]) ; 
 
+    int solve(vector<int>& nums, int curr_idx,
+              int prev, int n,
+              vector<vector<int>>& dp) {
 
-        for(int i = 1 ; i < n ; i++){
+        // Base case
+        if(curr_idx >= n)
+            return 0;
 
-            if(nums[i] > arr.back()) {
-                arr.push_back(nums[i]) ;
-            }
+        // Memoization
+        if(dp[curr_idx][prev + 1] != -1)
+            return dp[curr_idx][prev + 1];
 
-            else{
-                int is = lower_bound(arr.begin() , arr.end() , nums[i]) - arr.begin(); 
+        // Not take is always possible
+        int not_taken =
+            solve(nums, curr_idx + 1, prev, n, dp);
 
-                arr[is] = nums[i];
+        int taken = 0;
 
-            }
+        // Take only if increasing
+        if(prev == -1 || nums[curr_idx] > nums[prev]) {
+
+            taken =
+                1 + solve(nums, curr_idx + 1,
+                          curr_idx, n, dp);
         }
 
-        return arr.size() ;
+        return dp[curr_idx][prev + 1] =
+            max(taken, not_taken);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<vector<int>> dp(
+            n, vector<int>(n + 1, -1)
+        );
+
+        return solve(nums, 0, -1, n, dp);
     }
 };
