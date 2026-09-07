636a2c7a445be71b29c25514d19f287fe1a9d96c
HarshRajrani <155759996+HarshRajrani@users.noreply.github.com>
2026-09-07 18:40:10 +0530

    Time: 42 ms (17.43%) | Memory: 17.5 MB (67.28%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,86 +1,39 @@
 class Solution {
 public:
-
-    // first  = LIS length
-    // second = number of ways
-    pair<int,int> solve(vector<int>& nums, int i, int j, int n,
-                        vector<vector<pair<int,int>>>& dp) {
-
-        // Base case
-        if(i == n)
-            return {0, 1};
-
-        // Already calculated
-        if(dp[i][j + 1].first != -1)
-            return dp[i][j + 1];
-
-
-        
-        // NOT TAKEN
-        
-        pair<int,int> not_taken =
-            solve(nums, i + 1, j, n, dp);
-
-
-        
-        // TAKEN
-        
-        pair<int,int> taken = {0, 0};
-
-        if(j == -1 || nums[i] > nums[j]) {
-
-            pair<int,int> temp =
-                solve(nums, i + 1, i, n, dp);
-
-            taken.first = 1 + temp.first;
-            taken.second = temp.second;
-        }
-
-
-        
-        // Compare
-        
-
-        pair<int,int> ans;
-
-        // Taken gives longer LIS
-        if(taken.first > not_taken.first) {
-
-            ans = taken;
-        }
-
-        // Not taken gives longer LIS
-        else if(not_taken.first > taken.first) {
-
-            ans = not_taken;
-        }
-
-        // Both give same length
-        else {
-
-            ans.first = taken.first;
-
-            ans.second =
-                taken.second + not_taken.second;
-        }
-
-
-        return dp[i][j + 1] = ans;
-    }
-
-
     int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size() ; 
 
-        int n = nums.size();
+        vector<int>dp(n , 1) ;
+          vector<int>ct(n , 1) ;
+          int maxi = 1 ;
 
-        vector<vector<pair<int,int>>> dp(
-            n,
-            vector<pair<int,int>>(n + 1, {-1, -1})
-        );
+          for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j< i; j++ ){
 
-        pair<int,int> ans =
-            solve(nums, 0, -1, n, dp);
+                if(nums[j] < nums[i] && dp[j]+1 >dp[i]){
+                    dp[i] = dp[j] + 1 ; 
 
-        return ans.second;
+                    ct[i] = ct[j] ;
+                }
+
+                else if( nums[j] < nums[i] && dp[j] + 1 == dp[i]){
+                     ct[i] = ct[i] + ct[j];
+                }
+            }
+               maxi = max(maxi, dp[i]);
+            
+          }
+
+          int countLIS = 0 ;
+
+          for(int i = 0 ; i < n ; i++){
+
+            if (dp[i] == maxi) {
+                countLIS += ct[i] ;
+            }
+          }
+
+          return countLIS ;
+        
     }
 };
